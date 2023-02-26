303db1c0734ccc842d4d2570aecbf57b31549411
su10 <1687366+su10@users.noreply.github.com>
2023-02-26 23:09:15 +0900

    DOTweenインテグレーションにおけるTweenCancelBehaviourのデフォルトの指定値をKillからKillAndCancelAwaitに変更

--- Assets/Plugins/UniTask Supplement/UniTask.DOTween/Scripts/DOTweenAsyncExtensions.Supplement.cs
+++ Assets/Plugins/UniTask Supplement/UniTask.DOTween/Scripts/DOTweenAsyncExtensions.Supplement.cs
@@ -6,34 +6,36 @@ namespace Cysharp.Threading.Tasks
 {
     public static partial class DOTweenAsyncExtensions
     {
+        private const TweenCancelBehaviour DefaultTweenCancelBehaviour = TweenCancelBehaviour.KillAndCancelAwait;
+
         public static UniTask ToUniTask(this Tween tween, CancellationToken cancellationToken)
         {
-            return ToUniTask(tween, TweenCancelBehaviour.Kill, cancellationToken);
+            return ToUniTask(tween, DefaultTweenCancelBehaviour, cancellationToken);
         }
 
         public static UniTask AwaitForComplete(this Tween tween, CancellationToken cancellationToken)
         {
-            return AwaitForComplete(tween, TweenCancelBehaviour.Kill, cancellationToken);
+            return AwaitForComplete(tween, DefaultTweenCancelBehaviour, cancellationToken);
         }
 
         public static UniTask AwaitForPause(this Tween tween, CancellationToken cancellationToken)
         {
-            return AwaitForPause(tween, TweenCancelBehaviour.Kill, cancellationToken);
+            return AwaitForPause(tween, DefaultTweenCancelBehaviour, cancellationToken);
         }
 
         public static UniTask AwaitForPlay(this Tween tween, CancellationToken cancellationToken)
         {
-            return AwaitForPlay(tween, TweenCancelBehaviour.Kill, cancellationToken);
+            return AwaitForPlay(tween, DefaultTweenCancelBehaviour, cancellationToken);
         }
 
         public static UniTask AwaitForRewind(this Tween tween, CancellationToken cancellationToken)
         {
-            return AwaitForRewind(tween, TweenCancelBehaviour.Kill, cancellationToken);
+            return AwaitForRewind(tween, DefaultTweenCancelBehaviour, cancellationToken);
         }
 
         public static UniTask AwaitForStepComplete(this Tween tween, CancellationToken cancellationToken)
         {
-            return AwaitForStepComplete(tween, TweenCancelBehaviour.Kill, cancellationToken);
+            return AwaitForStepComplete(tween, DefaultTweenCancelBehaviour, cancellationToken);
         }
     }
 }
